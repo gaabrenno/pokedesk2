aafcc73f19f62d9f78e3e860ffbd8949ee0a4092
gaabrenno <gaabrenno@gmail.com>
2024-02-11 19:52:59 -0300

    inicio do cadastro

--- lib/app/module/auth/cadastro/cadastro_page.dart
+++ lib/app/module/auth/cadastro/cadastro_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class CadastroPage extends StatefulWidget {
   const CadastroPage({Key? key}) : super(key: key);
 
@@ -7,14 +9,34 @@ class CadastroPage extends StatefulWidget {
 }
 
 class _CadastroPageState extends State<CadastroPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _firebaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro Page'),
       ),
-      body: Center(
-        child: Text('Cadastro Page'),
+      body: ListView(
+        padding: EdgeInsets.all(12),
+        children: [
+          TextFormField(
+            controller: _nameController,
+            decoration: InputDecoration(label: Text('Nome Completo')),
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(label: Text('E-mail')),
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(label: Text('PassWord')),
+          ),
+          ElevatedButton(onPressed: (){}, child: Text('Cadastrar'))
+        ],
       ),
     );
   }
